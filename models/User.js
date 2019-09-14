4f67ea8be4f9d225eb7b3a553106bf20187cb114
Joshua Kelly <jdkelly2015@gmail.com>
2019-09-14 13:07:40 -0400

    created User Models using sequelize

---
+++ models/User.js
@@ -0,0 +1,19 @@
+module.exports = function (sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    // Giving the Author model a name of type STRING
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+  });
+
+  User.associate = models => {
+    User.hasMany(models.TimeSheet,
+      {
+        onDelete: "cascade"
+      })
+  }
+
+
+  return User;
+};
