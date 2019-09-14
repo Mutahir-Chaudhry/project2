002a4df623daeb22e1b5e1f444ef9ca536f2e1e7
Joshua Kelly <jdkelly2015@gmail.com>
2019-09-14 13:09:31 -0400

    created this file to add time sheet model using sequelize

---
+++ models/timeSheet.js
@@ -0,0 +1,36 @@
+module.exports = function(sequelize, DataTypes) {
+  var TimeSheet = sequelize.define("TimeSheet", {
+    startTime: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    endTime: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    }
+    // workOrderId: {
+    //   type: DataTypes.INTEGER,
+    //   allowNull: false
+    // },
+    // userId: {
+    //   type: DataTypes.INTEGER,
+    //   allowNull: false
+    // }
+  });
+
+  TimeSheet.associate = function(models) {
+    // We're saying that a Post should belong to an Author
+    // A Post can't be created without an Author due to the foreign key constraint
+    TimeSheet.belongsTo(models.WorkOrder, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  return TimeSheet;
+};
