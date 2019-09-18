3cd16bd74843fb473bcccf046a13c73d5388f11e
Mutahir-Chaudhry <mutachaudhry@gmail.com>
2019-09-17 21:33:27 -0400

    Working fr this time

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,39 +2,45 @@ var db = require("../models");
 
 module.exports = function(app) {
   //Get all Work Orders
-  app.get("/api/allworkorders", function(req, res) {
-    db.WorkOrder.findAll({}).then(function(results) {
-      res.json(results);
+  app.get("/api/workOrders", function(req, res) {
+    db.WorkOrder.findAll({}).then(function(dbWorkOrders) {
+      res.json(dbWorkOrders);
     });
   });
 
-  // // Get all Time Sheets
-  app.get("/api/alltimesheets", function(req, res) {
-    db.TimeSheet.findAll({}).then(function(results) {
-      res.json(results);
+  app.post("/api/workOrders", function(req, res) {
+    db.WorkOrder.create(req.body).then(function(dbWorkOrders) {
+      res.json(dbWorkOrders);
     });
   });
 
-  // // Get all Users
-  app.get("/api/allusers", function(req, res) {
-    db.User.findAll({}).then(function(results) {
-      res.json(results);
-    });
-  });
+  // // // Get all Time Sheets
+  // app.get("/api/alltimesheets", function(req, res) {
+  //   db.TimeSheet.findAll({}).then(function(results) {
+  //     res.json(results);
+  //   });
+  // });
 
-  // Create a new Work Order
-  app.post("/api/newworkorder", function(req, res) {
-    db.WorkOrder.post(req.body).then(function(results) {
-      res.json(results);
-    });
-  });
+  // // // Get all Users
+  // app.get("/api/allusers", function(req, res) {
+  //   db.User.findAll({}).then(function(results) {
+  //     res.json(results);
+  //   });
+  // });
 
-  // Delete a Work Order
-  app.delete("/api/workorders/:id", function(req, res) {
-    db.WorkOrder.destroy({ where: { id: req.params.id } }).then(function(
-      results
-    ) {
-      res.json(results);
-    });
-  });
+  // // Create a new Work Order
+  // app.post("/api/workOrders", function(req, res) {
+  //   db.WorkOrder.post(req.body).then(function(results) {
+  //     res.json(results);
+  //   });
+  // });
+
+  // // Delete a Work Order
+  // app.delete("/api/workorders/:id", function(req, res) {
+  //   db.WorkOrder.destroy({ where: { id: req.params.id } }).then(function(
+  //     results
+  //   ) {
+  //     res.json(results);
+  //   });
+  // });
 };
